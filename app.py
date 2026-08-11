354a942ab8974672875faf12ece95ab75d50bd79
SG-1022 <spg222020@gmail.com>
2026-08-11 14:36:39 -0500

    Initial Commit

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.title("Band Name Generator")
+
+with st.form(key="Band Name Generator"):
+
+    city = st.text_input("Enter your city")
+    pet_name = st.text_input("Enter your pet name")
+
+
+    submit_button = st.form_submit_button(label="Submit")
+
+if submit_button:
+    if city and pet_name:
+        f"Your band name could be: "
+        st.code(f"{city} {pet_name} Rockstars".title())
+    else:
+        st.exception(ValueError("Please fully fill the form."))
